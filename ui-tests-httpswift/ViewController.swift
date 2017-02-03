60740d2ef1a6d85812272141d1d4f498caabc289
Deline Neo <deline@users.noreply.github.com>
2017-02-03 20:23:17 +1100

    Initial commit

---
+++ ui-tests-httpswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ui-tests-httpswift
+//
+//  Created by Deline Neo on 3/02/2017.
+//  Copyright © 2017 Deline Neo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
